efd70de61dbc19764a752ac9ce7c37a3f141d970
Dustin Morrill <dmorrill10@gmail.com>
2021-01-18 15:35:09 -0700

    Improve plotting setup functions

--- mal_cfr_data/plot.py
+++ mal_cfr_data/plot.py
@@ -6,16 +6,25 @@ def plt_use_tex():
   params = {
       'text.usetex':
           True,
-      'text.latex.preamble': [
+      'text.latex.preamble': ''.join([
           r'\usepackage{amsmath}', r'\usepackage{nicefrac}',
           r'\usepackage[group-separator={,}]{siunitx}'
-      ]
+      ])
   }
   plt.rcParams.update(params)
 
 
-def set_plt_params():
+def set_plt_params(use_serif=True, font_size=24):
+  sns.set_style("white")
+  sns.set_style("ticks")
   plt_use_tex()
+  font_params = {'size': font_size}
+  if use_serif:
+    font_params['family'] = 'serif'
+  plt.rc('font', **font_params)
+  plt.rc('pdf', fonttype=42)
+  plt.rc('text', color='0')
+  plt.rc('axes', labelcolor='0')
 
 
 def diverging_color_palette():
