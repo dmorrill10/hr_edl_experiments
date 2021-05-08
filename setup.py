0a24581aba57a55e2c4a315062d121a5b8d6d1e3
Dustin Morrill <dmorrill10@gmail.com>
2021-05-08 09:00:25 -0600

    Rename python library and add dependency versions

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='mal_cfr_data',
+    name='hr_edl_data',
     version='0.0.1',
     license='',
     packages=find_packages(),
     install_requires=[
         'setuptools >= 20.2.2',
-        'absl-py',
-        'matplotlib',
-        'seaborn',
-        'numpy',
-        'pandas',
+        'absl-py == 0.9.0',
+        'matplotlib == 3.3.2',
+        'seaborn == 0.9.0',
+        'numpy == 1.20.2',
+        'pandas == 1.1.1',
+        'jupyter == 1.0.0',
     ],
 )
